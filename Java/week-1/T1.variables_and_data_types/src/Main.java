58eeaaf8d9a8350a5be654a26470d1df4e66b16b
Menna Hassan <h.menna85@gmail.com>
2024-04-15 02:08:05 +0200

    task 1

---
+++ Java/week-1/T1.variables_and_data_types/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        boolean rain = true;
+        String city = "alexandria";
+        int age = 18;
+        double height = 30.5;
+    }
+}
