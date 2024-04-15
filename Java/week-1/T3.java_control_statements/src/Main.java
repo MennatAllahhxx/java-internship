06e26a8dcada493dd34143bd29a98de8545a3d39
Menna Hassan <h.menna85@gmail.com>
2024-04-15 03:05:55 +0200

    task 3

---
+++ Java/week-1/T3.java_control_statements/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("even numbers between 1 and 20:");
+        for (int i = 2; i <= 20; i = i+2) {
+            System.out.println(i);
+        }
+    }
+}
