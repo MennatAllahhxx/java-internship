9276905799503eec3c2c939be60a2ddb44fe4913
Menna Hassan <h.menna85@gmail.com>
2024-07-26 14:50:18 +0300

    mysql task complete

--- JDBCTasks/T2.employee_application/src/Main.java
+++ JDBCTasks/T2.employee_application/src/Main.java
@@ -2,8 +2,6 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         MySQL mySQL = new MySQL();
@@ -15,13 +13,15 @@ public class Main {
         mySQL.createTable(connection, tableName);
 
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("ahmed", "mohamed", "Male", 30, "alexandria", "0111111111", 15));
+        employees.add(new Employee("ahmed", "mohamed", "Male", 30, "alexandria", "0111111111", 7));
         employees.add(new Employee("samy", "mahmoud"));
-        employees.add(new Employee("menna", "hassan", "female", 23, "alexandria", "0111111111"));
+        employees.add(new Employee("menna", "hassan", "female", 50, "alexandria", "0111111111"));
         employees.add(new Employee("layla", "farouk"));
-        employees.add(new Employee("sandy", "sayed", "female", 27, "cairo", "0111111222", 7));
+        employees.add(new Employee("samy", "sayed", "male", 46, "cairo", "0111111222", 15));
         employees.add(new Employee("samir", "ali"));
 
         mySQL.insertIntoTable(connection, tableName, employees);
+
+        mySQL.updateTable(connection, tableName);
     }
 }
--- JDBCTasks/T2.employee_application/src/MySQL.java
+++ JDBCTasks/T2.employee_application/src/MySQL.java
@@ -67,18 +67,22 @@ public class MySQL {
     }
 
     public void createTable(Connection connection, String tableName) {
-        setSqlStatement("CREATE TABLE " + tableName +" (" +
-                "Id int NOT NULL AUTO_INCREMENT, " +
-                "F_Name varchar(100) NOT NULL, " +
-                "L_Name varchar(100) NOT NULL, " +
-                "Sex varchar(50), " +
-                "Age int, " +
-                "Address varchar(255), " +
-                "Phone_Number varchar(255), " +
-                "Vacation_Balance int DEFAULT 30, " +
-                "PRIMARY KEY (Id));");
         try {
             Statement statement = connection.createStatement();
+
+            setSqlStatement("DROP TABLE IF EXISTS " + tableName + ";");
+            statement.executeUpdate(getSqlStatement());
+
+            setSqlStatement("CREATE TABLE " + tableName +" (" +
+                    "Id int NOT NULL AUTO_INCREMENT, " +
+                    "F_Name varchar(100) NOT NULL, " +
+                    "L_Name varchar(100) NOT NULL, " +
+                    "Sex varchar(50), " +
+                    "Age int, " +
+                    "Address varchar(255), " +
+                    "Phone_Number varchar(255), " +
+                    "Vacation_Balance int DEFAULT 30, " +
+                    "PRIMARY KEY (Id));");
             statement.executeUpdate(getSqlStatement());
 
             setMessage("Table created successfully");
@@ -140,5 +144,29 @@ public class MySQL {
             setMessage("Error adding the employee");
             System.out.println(getMessage());
         }
+    }
+
+    public void updateTable(Connection connection, String tableName) {
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            setSqlStatement("UPDATE " + tableName +
+                    " SET Vacation_Balance = 45 WHERE Age > 45");
+            statement.executeUpdate(getSqlStatement());
+
+            setSqlStatement("UPDATE " + tableName +
+                    " SET F_Name = 'Mr.' WHERE Age > 45 AND Sex = 'Male'");
+            statement.executeUpdate(getSqlStatement());
+
+            setSqlStatement("UPDATE " + tableName +
+                    " SET F_Name = 'Mrs.' WHERE Age > 45 AND Sex = 'Female'");
+            statement.executeUpdate(getSqlStatement());
+
+            setMessage("Table updated successfully");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            setMessage("Error updating the table");
+        }
+        System.out.println(getMessage());
     }
 }
